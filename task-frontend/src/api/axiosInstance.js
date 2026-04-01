e0245dfd0fea4722f0613c3fc72954e9bb9878c0
saimon-35 <zia73575@gmail.com>
2026-04-01 12:23:57 +0600

    fix: update API URL handling to support environment variable

--- task-frontend/src/api/axiosInstance.js
+++ task-frontend/src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://127.0.0.1:5000";
+const API = process.env.REACT_APP_API_URL || "http://127.0.0.1:5000";
 
 const axiosInstance = axios.create({
   baseURL: API
